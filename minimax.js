ca43990a2fe2686ab76c6f7cd5e97adb02b6057c
Maxime Joly <maxime.joly2808@gmail.com>
2020-07-05 15:28:59 +0200

    First commit

---
+++ minimax.js
@@ -0,0 +1,74 @@
+let MAX_DEPTH = 5;
+
+function getBestMove() {
+  let bestScore = -Infinity;
+  let bestMoves = []; // best moves found
+
+  // If the board is empty, choose a random column
+  if (isEmptyBoard()) {
+    let col = round(random() * boardCol);
+    return { col };
+  }
+
+  for (let i = 0; i < boardCol; i++) {
+    // If the column if not full
+    if (board[i][boardRow - 1] === -Infinity) {
+      pushPiece(i, players.AI);
+      let score = minimax(board, 0, false);
+      removePiece(i);
+      if (score > bestScore) {
+        bestMoves = [];
+        bestScore = score;
+      }
+      if (score === bestScore) {
+        bestMoves.push({ col: i });
+      }
+    }
+  }
+
+  if (bestMoves.length > 1) {
+    let rand = floor(random() * bestMoves.length);
+    return bestMoves[rand];
+  } else {
+    return bestMoves[0];
+  }
+}
+
+function minimax(board, depth, isMaximizingPlayer) {
+  if (depth > MAX_DEPTH) {
+    return 0;
+  }
+
+  let result = checkWinner();
+  if (result) {
+    if (result === 'none') return 0;
+    if (result === players.HUMAN) return -10;
+    if (result === players.AI) return 10;
+  }
+
+  if (isMaximizingPlayer) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < boardCol; i++) {
+      // If the column if not full
+      if (board[i][boardRow - 1] === -Infinity) {
+        pushPiece(i, players.AI);
+        let score = minimax(board, depth + 1, false);
+        removePiece(i);
+        bestScore = max(score, bestScore);
+      }
+    }
+    return bestScore;
+  } else {
+    let bestScore = Infinity;
+    for (let i = 0; i < boardCol; i++) {
+      // If the column if not full
+      if (board[i][boardRow - 1] === -Infinity) {
+        pushPiece(i, players.HUMAN);
+        let score = minimax(board, depth + 1, true);
+        removePiece(i);
+        bestScore = min(score, bestScore);
+      }
+    }
+    return bestScore;
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,210 @@
+let CW = 700;
+let CH = 600;
+
+let board = [];
+let boardRow = 6;
+let boardCol = 7;
+
+let players = { HUMAN: 'red', AI: 'orange' };
+let currentPlayer;
+let end = false;
+
+function setup() {
+  createCanvas(CW, CH);
+  reset();
+}
+
+function reset() {
+  board = new Array(boardCol).fill(-Infinity);
+  for (let i = 0; i < board.length; i++) {
+    board[i] = new Array(boardRow).fill(-Infinity);
+  }
+  end = false;
+  currentPlayer = random() > 0.5 ? players.HUMAN : players.AI;
+  if (currentPlayer === players.AI) {
+    AI();
+  }
+}
+
+function draw() {
+  background(255);
+  drawBoard();
+
+  let result = checkWinner();
+  if (result) {
+    noLoop();
+    if (result === players.AI) {
+      fill('red');
+      textSize(30);
+      textAlign(CENTER);
+      text('AI wins !', CW / 2, CH / 2);
+    }
+    if (result === players.HUMAN) {
+      fill('green');
+      textSize(30);
+      textAlign(CENTER);
+      text('You win !', CW / 2, CH / 2);
+    }
+  }
+}
+
+function drawBoard() {
+  let w = CW / boardCol;
+  let h = CH / boardRow;
+
+  stroke(0);
+  strokeWeight(2);
+
+  // Draw the lines
+  for (let i = 0; i <= boardCol; i++) {
+    line(i * w, 0, i * w, CH);
+  }
+  line(0, CH, CW, CH);
+
+  // Draw the pieces
+  for (let i = 0; i < boardCol; i++) {
+    for (let j = 0; j < boardRow; j++) {
+      let centerX = i * w + w / 2;
+      let centerY = CH - j * h - h / 2;
+      if (board[i][j] === players.HUMAN) {
+        fill('red');
+        ellipse(centerX, centerY, w, h);
+      }
+      if (board[i][j] === players.AI) {
+        fill('orange');
+        ellipse(centerX, centerY, w, h);
+      }
+    }
+  }
+}
+
+function mousePressed() {
+  let w = CW / boardCol;
+  if (!end && currentPlayer === players.HUMAN) {
+    let col = floor(mouseX / w);
+    // If the column is not full
+    if (board[col][boardRow - 1] === -Infinity) {
+      pushPiece(col, players.HUMAN);
+      currentPlayer = players.AI;
+      let result = checkWinner();
+      if (result) {
+        end = true;
+      } else {
+        AI();
+      }
+    }
+  }
+}
+
+function keyPressed() {
+  if (end && keyCode === ENTER) {
+    reset();
+    redraw();
+  }
+}
+
+function AI() {
+  if (currentPlayer === players.AI) {
+    let bestMove = getBestMove();
+    pushPiece(bestMove.col, players.AI);
+    currentPlayer = players.HUMAN;
+    let result = checkWinner();
+    if (result) {
+      end = true;
+    }
+  }
+}
+
+function pushPiece(col, player) {
+  let j = 0;
+  // Find the j index to place the piece vertically
+  while (board[col][j] !== -Infinity && j < boardRow) {
+    j++;
+  }
+  if (j < boardRow) {
+    board[col][j] = player;
+  }
+}
+
+function removePiece(col) {
+  let j = boardRow - 1;
+  // Find the j index to place the piece vertically
+  while (board[col][j] === -Infinity && j >= 0) {
+    j--;
+  }
+  if (j > -1) {
+    board[col][j] = -Infinity;
+  }
+}
+
+function isFullBoard() {
+  for (let i = 0; i < boardCol; i++) {
+    for (let j = 0; j < boardRow - 3; j++) {
+      if (board[i][j] === -Infinity) return false;
+    }
+  }
+  return true;
+}
+
+function isEmptyBoard() {
+  for (let i = 0; i < boardCol; i++) {
+    for (let j = 0; j < boardRow - 3; j++) {
+      if (board[i][j] !== -Infinity) return false;
+    }
+  }
+  return true;
+}
+
+function equals(a, b, c, d) {
+  return a !== -Infinity && a === b && b === c && c === d;
+}
+
+function checkWinner() {
+  let winner = null;
+
+  // Check horizontally
+  for (let j = 0; j < boardRow; j++) {
+    for (let i = 0; i < boardCol - 3; i++) {
+      if (equals(board[i][j], board[i + 1][j], board[i + 2][j], board[i + 3][j])) {
+        winner = board[i][j];
+      }
+    }
+  }
+
+  // Check vertically
+  for (let i = 0; i < boardCol; i++) {
+    for (let j = 0; j < boardRow - 3; j++) {
+      if (equals(board[i][j], board[i][j + 1], board[i][j + 2], board[i][j + 3])) {
+        winner = board[i][j];
+      }
+    }
+  }
+
+  // Check diagonales (left bottom - right top)
+  for (let j = 0; j < boardRow - 3; j++) {
+    for (let i = 0; i < boardCol - 3; i++) {
+      if (
+        equals(board[i][j], board[i + 1][j + 1], board[i + 2][j + 2], board[i + 3][j + 3])
+      ) {
+        winner = board[i][j];
+      }
+    }
+  }
+
+  // Check diagonales (left top - right bottom)
+  for (let j = 3; j < boardRow; j++) {
+    for (let i = 0; i < boardCol - 3; i++) {
+      if (
+        equals(board[i][j], board[i + 1][j - 1], board[i + 2][j - 2], board[i + 3][j - 3])
+      ) {
+        winner = board[i][j];
+      }
+    }
+  }
+
+  if (!winner && isFullBoard()) {
+    return 'none';
+  } else {
+    return winner;
+  }
+}
